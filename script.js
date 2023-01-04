7a5f39e965d8612be5ecda64d0d65702dbe15c88
Wayne Tucker <95285849+waynetuckermedia@users.noreply.github.com>
2023-01-04 15:56:25 -0500

    Manipulation

--- script.js
+++ script.js
@@ -17,32 +17,77 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
+const title = document.querySelector("#main-title");
+
+title.textContent = "Dom's Movie";
 
 
   // Part 2
-
+document.body.style.backgroundColor = "red";
 
   // Part 3
+function removeLastFav() {
+  const lastFavThing = document.querySelector("#favorite-things li:last-child");
+  lastFavThing.parentNode.removeChild(lastFavThing);
+}
+removeLastFav()
+removeLastFav()
 
 
   // Part 4
 
+  const specialTitle = document.querySelectorAll(".special-title");
+
+ for(let i of specialTitle){
+  i.style.fontSize = "2rem";
+ } 
 
   // Part 5
-
+const pastRaces = document.querySelectorAll("#past-races li");
+console.log(pastRaces);
+pastRaces[3].remove();
 
   // Part 6
+const newRaces = document.querySelector("#past-races");
+const newCity = document.createElement("LI");
+
+newCity.textContent = "NYC";
+newRaces.append(newCity);
 
 
   // Part 7
+const main = document.querySelector(".main")
+
+const blogPost = document.createElement("DIV");
+blogPost.classList.add("blog-post");
+
+const blogPostH2 = document.createElement("H2");
+blogPostH2.textContent = "This is the blog";
+
+const newParagraph = document.createElement("P");
+newParagraph.textContent = "Bla Bla";
+
+main.appendChild(blogPost)
+blogPost.appendChild(blogPostH2)
+blogPost.appendChild(newParagraph)
+blogPost.classList.add("purple")
 
 
   // Part 8
-
-
-  // Part 9
-
-
-
-
+const quoteTitle = document.querySelector("#quote-title");
+quoteTitle.addEventListener('click', function() {
+  randomQuote();
 });
+
+//   // Part 9
+const blogPosts = document.querySelectorAll('.blog-post');
+blogPosts.forEach((posts) => {
+  posts.addEventListener("mouseout", function (event) {
+    event.currentTarget.classList.toggle("purple")
+  });
+  posts.addEventListener("mousein", (event) => {
+    event.stopPropagation()
+    event.currentTarget.classList.toggle("red");
+  })
+})
+ });
